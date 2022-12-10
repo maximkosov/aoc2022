237a6d9f57f6c352f944f86e8819bad75e3e6176
Maxim Kosov <maxim@retailnext.net>
2022-12-10 22:07:55 +0400

    Make overlaps function transitive
    Also add error handling which is not really needed in aoc but anyways.

--- day4/main.go
+++ day4/main.go
@@ -24,12 +24,15 @@ func main() {
 	f, _ := os.Open(*inputFile)
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		ranges := strings.Split(scanner.Text(), ",")
-		r1, r2 := parseRange(ranges[0]), parseRange(ranges[1])
+		r1, r2, err := parseRanges(scanner.Text())
+		if err != nil {
+			panic(err)
+		}
+
 		if r1.contains(r2) || r2.contains(r1) {
 			nContains++
 		}
-		if r1.overlaps(r2) || r2.overlaps(r1) {
+		if r1.overlaps(r2) {
 			nOverlaps++
 		}
 	}
@@ -46,12 +49,41 @@ func (r assignedRange) contains(other assignedRange) bool {
 }
 
 func (r assignedRange) overlaps(other assignedRange) bool {
-	return r.start >= other.start && r.start <= other.end
+	return r.start >= other.start && r.start <= other.end ||
+		other.start >= r.start && other.start <= r.end
 }
 
-func parseRange(text string) assignedRange {
+func parseRanges(text string) (assignedRange, assignedRange, error) {
+	ranges := strings.Split(text, ",")
+	if len(ranges) != 2 {
+		return assignedRange{}, assignedRange{}, fmt.Errorf("cannot parse ranges from %s", text)
+	}
+
+	r1, err := parseRange(ranges[0])
+	if err != nil {
+		return assignedRange{}, assignedRange{}, err
+	}
+
+	r2, err := parseRange(ranges[1])
+	if err != nil {
+		return assignedRange{}, assignedRange{}, err
+	}
+
+	return r1, r2, nil
+}
+
+func parseRange(text string) (assignedRange, error) {
 	startEnd := strings.Split(text, "-")
-	start, _ := strconv.Atoi(startEnd[0])
-	end, _ := strconv.Atoi(startEnd[1])
-	return assignedRange{start: start, end: end}
+	if len(startEnd) != 2 {
+		return assignedRange{}, fmt.Errorf("cannot parse range from %s", startEnd)
+	}
+	start, err := strconv.Atoi(startEnd[0])
+	if err != nil {
+		return assignedRange{}, fmt.Errorf("cannot parse start: %w", err)
+	}
+	end, err := strconv.Atoi(startEnd[1])
+	if err != nil {
+		return assignedRange{}, fmt.Errorf("cannot parse end: %w", err)
+	}
+	return assignedRange{start: start, end: end}, nil
 }
